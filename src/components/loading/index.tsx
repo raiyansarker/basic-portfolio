1f1283956fac63f5c8309be229e64652f341dd59
Raiyan Sarker <developerraiyan@gmail.com>
2024-11-15 15:59:16 +0600

    changers to loading component

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -4,8 +4,8 @@ import styles from "./loading.module.css";
 
 export default function LoadingSpinner({ className }: { className?: string }) {
   return (
-    <div className={cn("size", className)}>
-      <div className={cn(styles.spinner, "size", className)}>
+    <div className={cn("size-5", className)}>
+      <div className={cn(styles.spinner, "size-5", className)}>
         {[...Array(12)].map((_, i) => (
           <div key={i} />
         ))}
